ad4144495207d32077ddd9c798de45fd5b2ccb9b
Кристина Гордийчук <gordiichukchristina@yandex.ru>
2024-11-28 13:41:05 +0300

    Test 4

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'backend_static/'
+STATIC_ROOT = BASE_DIR / 'main.yml/'
 
 
 MEDIA_URL = '/media/'
