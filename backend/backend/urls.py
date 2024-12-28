524125a9e9f5ff6a68f83419362c762c4e82e5c1
Кристина <161318967+Christina-Gordiichuk@users.noreply.github.com>
2024-12-28 15:35:25 +0400

    Update urls.py
    fixed pep8 errors in the file (trailing whitespaces)

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -26,10 +26,10 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(
-        settings.STATIC_URL, 
+        settings.STATIC_URL,
         document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(
-        settings.MEDIA_URL, 
+        settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT)
