663795eb970929383cd09acecd58dd20e2144f57
Кристина <161318967+Christina-Gordiichuk@users.noreply.github.com>
2024-12-28 13:11:11 +0400

    Update models.py
    fixed pep8 errors in the file

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.contrib.auth import get_user_model
-from django.db import models, migrations
+from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 from ingredients.models import Ingredient
